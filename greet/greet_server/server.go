48dedd089ffca57ef5d01177d57f6fb7ab22caba
Bryon Larrance <Bryon.Larrance@smiledirectclub.com>
2019-03-30 17:49:50 -0500

    Create boilerplate for client and server

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -2,25 +2,25 @@ package main
 
 import (
   "log"
-   "net"
-   "fmt"
-   "google.golang.org/grpc"
-   "github.com/beelarr/grpc-go-course/greet/greetpb"
+  "net"
+  "fmt"
+  "google.golang.org/grpc"
+  "grpc-go-course/greet/greetpb"
    )
 
 type server struct{}
 
 func main() {
   fmt.Println("Hello World")
-  lis, err := net.Listen("tcp", " 0.0.0.0:50051"
+  lis, err := net.Listen("tcp", "0.0.0.0:50051")
   if err != nil {
     log.Fatalf("Failed to listen: %v", err)
   }
 
   s:=grpc.NewServer()
-  greetpb.RegistewrGreetSErviceServer(s, &server{})
+  greetpb.RegisterGreetServiceServer(s, &server{})
 
-  if err := s.Server(lis); err != nil {
+  if err := s.Serve(lis); err != nil {
     log.Fatalf("failed to server: %v", err)
   }
 }
